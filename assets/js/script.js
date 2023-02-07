f98adf71b4d8c62a9e475e839ab5c308bd7d393b
Cavid Bashirov <bashirovcavid93gmail.com>
2023-02-07 15:48:17 +0400

    initial

---
+++ assets/js/script.js
@@ -0,0 +1,281 @@
+"use strict";
+
+
+
+
+// let num = 8;
+
+
+// if(num%2==0){
+//     console.log("Cutdur")
+// }else{
+//     console.log("Tekdir")
+// }
+
+
+// let n = 30;
+// let count = 0;
+
+// for (let i = 0; i <= n; i++) {
+
+//     if (i % 2 == 1) {
+//         count ++;
+//     }
+
+// }
+
+// console.log(count)
+
+
+
+
+// let arr = [6,9, "Konul", "100"];
+
+// for(let i=0; i < arr.length;i++){
+
+//     debugger
+
+//     if(!isNaN(arr[i]) && typeof arr[i]== "number"){
+
+//         if(arr[i] % 2 == 0){
+//             console.log(arr[i])
+//         }
+//     }
+
+// }
+
+
+
+
+
+// try {
+
+//     let isMarried = true;
+
+//     if(isMarried){
+//         let surname  = "XXX";
+//         console.log(surname)
+//     }
+
+
+
+// } catch (error) {
+
+
+//     console.log(error.message)
+// }finally{
+//     console.log("Welcome")
+// }
+
+
+
+// let name = "YYY";
+
+// if(true){
+//     let surname = "XXX"
+
+//     console.log(surname)
+// }
+
+
+
+// function showName(){
+//     console.log("Cavid");
+// }
+
+
+
+// function  showName(){
+//     return "Cavid";
+// }
+
+// showNumbers(3, 7)
+
+// function sumOfNums(a,c,num3){
+//     return c + a + num3;
+// }
+
+
+// let result = sumOfNums(55,10,3);
+
+// console.log(result)
+
+
+// function showNumbers(n, m) {
+//     debugger
+//     for (let i = n; i < m; i++) {
+//         if (i == 8) {
+//             return;
+//         }
+//     }
+
+//     console.log(n + m);
+// }
+
+
+// function showNumbers(n, m) {
+//     for (let i = n; i < m; i++) {
+//         if (i == 5) {
+
+//             continue;
+//         }
+
+//         console.log(i)
+//     }
+
+//     console.log(n + m);
+// }
+
+
+
+// function getNumbers(){
+//     console.log(arguments);
+// }
+
+
+// getNumbers(1,2,3,4)
+
+// function getNumbers(){
+//     let nums = arguments;
+
+//     for(let i = 0; i < nums.length;i++){
+//         console.log(nums[i])
+//     }
+// }
+
+// getNumbers(1,2,3)
+
+
+// function login(){
+//     console.log(this)
+// }
+
+
+
+
+// let create = (text, number) => {
+//     console.log(text + "/" + number)
+// }
+
+// create("salamlar",50);
+
+
+// let create = () => {
+//   console.log(arguments)
+// }
+
+
+// create(1,2,3);
+
+
+
+// function test(){
+//     console.log(arguments)
+// }
+
+// test(1,2,34);
+
+
+// function test(){
+//     console.log(this)
+// }
+
+// let test1 = () =>{
+//     console.log(this)
+// }
+
+// test1();
+// test();
+
+// let student = {
+
+//     name: "Pervin",
+//     surname: "Rehimli",
+//     age: 26,
+
+// }
+
+// let test = ()=>{
+//     console.log(this)
+// }
+
+
+// student.getData(1,2,3);
+
+// let nums = [1,2,3,4,5];
+
+
+// let showOddNumbers = (arr) =>{
+//   for(let i = 0;i<arr.length;i++){
+//     if(arr[i]%2 == 1){
+//         console.log(arr[i])
+//     }
+//   }
+// }
+
+// showOddNumbers(nums);
+
+
+// let nums = [2,3,4,5,6,7];
+
+// showOddNums(nums)
+
+// function showOddNums(arr){
+//     for(let i = 0;i<arr.length;i++){
+//         if(arr[i]%2 == 1){
+//             console.log(arr[i])
+//         }
+//       }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
